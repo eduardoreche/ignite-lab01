1f6b893db6774fc2d6840335e5f7ee364f87d0e1
Eduardo Reche Lopes <eduardo.rechelopes@exxonmobil.com>
2022-04-16 14:24:17 -0300

    feat: list customer data

--- purchases/src/http/http.module.ts
+++ purchases/src/http/http.module.ts
@@ -8,6 +8,7 @@ import { ProductsService } from 'src/services/products.service';
 import { PurchasesService } from 'src/services/purchases.service';
 
 import { DatabaseModule } from '../database/database.module';
+import { CustomersResolver } from './graphql/resolvers/customers.resolver';
 import { ProductsResolver } from './graphql/resolvers/products.resolver';
 import { PurchasesResolver } from './graphql/resolvers/purchases.resolver';
 
@@ -24,6 +25,7 @@ import { PurchasesResolver } from './graphql/resolvers/purchases.resolver';
     //resolvers
     ProductsResolver,
     PurchasesResolver,
+    CustomersResolver,
 
     //services
     ProductsService,
